a9c699079f3da4170b68d527bebaafeb751e5978
usernameKim <akddkwl1310@gmailcom>
2023-01-04 12:34:01 +0900

    vue init

---
+++ demo-1/front/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <empList></empList>
+  </div>
+</template>
+
+<script>
+import empList from './components/empList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    empList
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- demo-1/src/main/java/com/example/demo/emp/web/EmpController.java
+++ demo-1/src/main/java/com/example/demo/emp/web/EmpController.java
@@ -1,22 +1,34 @@
 package com.example.demo.emp.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.emp.mapper.EmpMapper;
+import com.example.demo.emp.service.EmpVO;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class EmpController {
 	
 	@Autowired
 	EmpMapper empMapper;
 	// 필요시에만 get or post 지정 
+	
 	@RequestMapping(value = "/empList", method = RequestMethod.GET) 
 	public String empList(Model model) {
 		model.addAttribute("empList", empMapper.getEmpList(null));
 		return "empList";
+	}
+	@RequestMapping(value = "/empInfoList", method = RequestMethod.GET)
+	@ResponseBody
+	public List<EmpVO> empInfoList(Model model){
+		return empMapper.getEmpList(null);
 	}
 }
